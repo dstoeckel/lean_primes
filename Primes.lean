d2f6601ae4e0ad64e3c38134d5a6ebcef9121c39
Daniel Stöckel <daniel@somweyr.de>
2024-12-27 20:24:46 +0100

    wip

--- Primes.lean
+++ Primes.lean
@@ -240,17 +240,32 @@ theorem mul_dvd {n m k: Nat} (h: n = m * k): m ∣ n ∧ k ∣ n := by
 def IsOdd (n: Nat) := ∃k, n = 2*k + 1
 def IsEven (n: Nat) := ∃k, n = 2*k
 
-def odd_is_not_even (n: Nat) (h_odd: IsOdd n): ¬IsEven n := by
+theorem odd_is_not_even (n: Nat) (h_odd: IsOdd n): ¬IsEven n := by
   intro h_even
   cases h_odd with
   | intro k hk => cases h_even with
     | intro l hl => omega
 
-def even_is_not_odd (n: Nat) (h_even: IsEven n): ¬IsOdd n := by
+theorem even_is_not_odd (n: Nat) (h_even: IsEven n): ¬IsOdd n := by
   intro h_odd
   cases h_odd with
   | intro k hk => cases h_even with
     | intro l hl => omega
+
+theorem even_eq_dvd_two {n: Nat}: IsEven n ↔ 2 ∣ n := by
+  constructor
+  . intros h
+    cases h with
+    | intro k hk =>
+      rw [hk]
+      apply Nat.dvd_mul_right
+  . intros h
+    cases h with
+    | intro k hk =>
+      rw [hk]
+      exists k
+
+theorem even_of_odd_succ {n: Nat} (hn: IsOdd n): IsEven (n + 1) := by sorry
 
 theorem even_not_prime (p: Nat) (hp: IsEven p) (h2: p > 2): ¬Prime p := by
   intro h
@@ -262,8 +277,14 @@ theorem even_not_prime (p: Nat) (hp: IsEven p) (h2: p > 2): ¬Prime p := by
     contradiction
 
 theorem not_even_is_odd (p: Nat) (hp: ¬IsEven p): IsOdd p := by
-  unfold IsOdd
-  sorry
+  match p with
+  | 0      => exfalso; apply hp; exists 0
+  | p' + 1 =>
+    rw [even_eq_dvd_two] at hp
+    exists p' / 2
+    have h2: 2 ∣ p' := by
+      omega
+    rw [Nat.mul_div_cancel' h2]
 
 theorem primes_are_odd (p: Nat) (hp: Prime p) (h2: 2 < p): IsOdd p := by
   apply not_even_is_odd
@@ -673,17 +694,43 @@ theorem zero_lt_factorial {n: Nat}: 0 < factorial n := by
   | zero => rw [factorial_0_is_1]; apply Nat.lt_add_one
   | succ n ih => rw [factorial_mul]; exact Nat.mul_pos (Nat.zero_lt_succ n) ih
 
+theorem le_factorial {n: Nat}: n ≤ factorial n := by
+  match n with
+  | 0 => rw [factorial_0_is_1]; exact Nat.zero_le 1
+  | i + 1 =>
+    rw [factorial_mul]
+    exact Nat.le_mul_of_pos_right (i + 1) zero_lt_factorial
+
+theorem lt_factorial {n: Nat} (h: 2 < n): n < factorial n := by
+  match n with
+  | 0 | 1 | 2 => contradiction
+  | i + 3 =>
+    rw [factorial_mul, Nat.add_assoc]
+    conv =>
+      left; rw [← Nat.mul_one (i + 3)]
+    apply Nat.mul_lt_mul_of_pos_left
+    case hk => omega
+    case h => induction i with
+      | zero => simp only [Nat.zero_add, factorial_2_is_2, Nat.lt_add_one]
+      | succ i hi =>
+        rw [factorial_mul]
+        calc
+          1 < factorial (i + 2) := hi (by omega)
+          _ < (i + 2) * factorial (i + 2) + factorial (i + 2) := by
+            apply Nat.lt_add_of_pos_left
+            exact Nat.mul_pos (by omega) (zero_lt_factorial)
+          _ = (i + 3) * factorial (i + 2) := by rw [←Nat.succ_mul]
+
 theorem dvd_factorial_of_le {n m: Nat} (h: m ≤ n) (hm: 0 < m): m ∣ factorial n := by
   induction n with
-  | zero => omega
+  | zero => rw [Nat.eq_zero_of_le_zero h] at hm; contradiction
   | succ n hi =>
     rw [factorial_mul]
     cases Nat.lt_or_eq_of_le h with
     | inl hlt =>
-      cases hi (Nat.le_of_lt_succ hlt) with
-      | intro l hl =>
-        exists l * (n + 1)
-        rw [←Nat.mul_assoc, Nat.mul_comm, hl]
+      rcases hi (Nat.le_of_lt_succ hlt) with ⟨l, hl⟩
+      exists l * (n + 1)
+      rw [Nat.mul_comm, hl, Nat.mul_assoc]
     | inr heq =>
       rw [heq]
       exists factorial n
@@ -743,36 +790,364 @@ decreasing_by
     rwa [Nat.mul_lt_mul_left (by omega)]
   omega
 
-
 theorem min_fac_pos {n: Nat}: 0 < minFac n := by
   unfold minFac
   split
   . apply Nat.succ_pos
   . apply min_fac_aux_pos
 
+theorem min_fac_aux_gt_2 {n k: Nat} (hk: 1 < k) (hn: 1 < n): 1 < minFacAux n k hk := by
+  unfold minFacAux
+  split
+  . assumption
+  . split
+    . assumption
+    . apply min_fac_aux_gt_2
+      exact hn
+termination_by n - k
+decreasing_by
+  -- This seems somewhat crude ...
+  have : k < k * k := by
+    conv => lhs; rw [←Nat.mul_one k]
+    rwa [Nat.mul_lt_mul_left (by omega)]
+  omega
+
+theorem min_fac_gt_2 {n: Nat} (h: 1 < n): 1 < minFac n := by
+  unfold minFac
+  split
+  . omega
+  . apply min_fac_aux_gt_2 (by omega) h
+
+theorem min_fac_aux_dvd {n k: Nat} (hk: 1 < k): minFacAux n k hk ∣ n := by
+  unfold minFacAux
+  split
+  . assumption
+  . split
+    . apply Nat.dvd_refl
+    . exact min_fac_aux_dvd (by omega)
+termination_by n - k
+decreasing_by
+  -- This seems somewhat crude ...
+  have : k < k * k := by
+    conv => lhs; rw [←Nat.mul_one k]
+    rwa [Nat.mul_lt_mul_left (by omega)]
+  omega
+
+theorem min_fac_aux_le {n k: Nat} (hn: 0 < n) (hk: 1 < k): minFacAux n k hk ≤ n :=
+  Nat.le_of_dvd hn (min_fac_aux_dvd hk)
+
+theorem min_fac_dvd {n: Nat}: minFac n ∣ n := by
+  unfold minFac
+  split
+  . assumption
+  . exact min_fac_aux_dvd (by omega)
+
+theorem dvd_of_min_fac_dvd {n m: Nat} (h: n ∣ minFac m): n ∣ m :=
+  Nat.dvd_trans h min_fac_dvd
+
+theorem min_fac_aux_val (n k: Nat) (hk: 1 < k): n = minFacAux n k hk ∨ k ≤ minFacAux n k hk := by
+  unfold minFacAux
+  split
+  . right
+    apply Nat.le_refl
+  . split
+    . left; rfl
+    . have bla := min_fac_aux_val n (k + 2) (by omega)
+      cases bla
+      . left; assumption
+      . right; omega
+termination_by n - k
+decreasing_by
+  -- This seems somewhat crude ...
+  have : k < k * k := by
+    conv => lhs; rw [←Nat.mul_one k]
+    rwa [Nat.mul_lt_mul_left (by omega)]
+  omega
+
+theorem not_dvd_sqrt {n m: Nat} (hn: 0 < n) (hmltn: m < n) (h: n < m * m) (hinv: ∀i, 1 < i → i < m → ¬ i ∣ n): ¬ m ∣ n := by
+  intro hmn
+  have hm := Nat.pos_of_dvd_of_pos hmn hn
+  cases hmn with
+  | intro k hk =>
+    rw [hk] at h
+    have h': k < m := Nat.lt_of_mul_lt_mul_left h
+    have hk': 0 < k := by rw [hk] at hn; exact Nat.pos_of_mul_pos_left hn
+    have hk'': k ≠ 1 := by
+      intro hk1
+      rw [hk1, Nat.mul_one] at hk
+      omega
+    apply hinv k (Nat.lt_of_le_of_ne hk' (Ne.symm hk'')) h'
+    exists m
+    rw [Nat.mul_comm]
+    assumption
+
+theorem odd_of_odd_plus_2 {n: Nat} (h: IsOdd n): IsOdd (n + 2) := by
+  rcases h with ⟨k, hk⟩
+  exists k + 1
+  omega
+
+theorem lt_sqr_add_of_lt_sql {n k l: Nat} (h: n < k * k): n < (k + l) * (k + l) := by
+  calc
+    n < k * k := by assumption
+    _ ≤ k * k + k * l + l * k + l * l := by omega
+    _ = k * (k + l) + l * k + l * l := by rw [Nat.mul_add]
+    _ = k * (k + l) + l * (k + l) := by rw [Nat.add_assoc, ←Nat.mul_add]
+    _ = (k + l) * (k + l) := by rw [Nat.add_mul]
+
+theorem mfa_lt_mfa_succ_succ {n k: Nat} (hn: 0 < n) (hk: 1 < k): minFacAux n k hk ≤ minFacAux n (k + 2) (by omega) := by
+  have hdvd: minFacAux n k hk ≤ n := min_fac_aux_le hn hk
+  have hdvd': minFacAux n (k + 2) (by omega) ≤ n  := min_fac_aux_le hn (by omega)
+
+  conv => left; unfold minFacAux
+  split
+  case isTrue hkn =>
+    unfold minFacAux
+    split
+    . omega
+    . split
+      . exact Nat.le_of_dvd hn hkn
+      . cases min_fac_aux_val n (k + 2 + 2) (by omega) with
+        | inl heq => rw [←heq]; exact Nat.le_of_dvd hn hkn;
+        | inr hle => calc
+          k ≤ k + 2 +2 := by omega
+          _ ≤ minFacAux n (k + 2 + 2) (by omega) := hle
+  case isFalse hnot_kn =>
+    split
+    case isTrue hlt =>
+      cases min_fac_aux_val n (k + 2) (by omega) with
+      | inl heq => rw [←heq]; exact Nat.le_refl n;
+      | inr hlt' =>
+        unfold minFacAux
+        split
+        case isTrue hdvd'' =>
+          have hk2 := Nat.le_of_dvd hn hdvd''
+          sorry
+
+        . have hlt'': n < (k + 2) * (k + 2) := lt_sqr_add_of_lt_sql hlt
+          split
+          . exact Nat.le_refl n
+          . contradiction
+    case isFalse hge => exact Nat.le_refl (minFacAux n (k + 2) (by omega))
+
+
+theorem not_dvd_of_sq_lt {n k: Nat} (h: n < k * k) (hall: ∀i, i ≤ k → ¬i ∣ n): ∀m, m > k → ¬ m ∣ n := by
+  rintro m hgt ⟨l, hl⟩
+  have hk_pos: 0 < k := Nat.pos_of_mul_pos_left (Nat.zero_lt_of_lt h)
+  have hlt: l ≤ k := by
+    false_or_by_contra
+    rename ¬l ≤ k => hgt'
+    replace hgt' := Nat.gt_of_not_le hgt'
+    have hl_pos: 0 < l := Nat.zero_lt_of_lt hgt'
+    rw [hl] at h
+    have h_contra: m * l < m * l := by calc
+      m * l < k * k := h
+      _ < k * l := (Nat.mul_lt_mul_left hk_pos).mpr hgt'
+      _ < m * l := (Nat.mul_lt_mul_right hl_pos).mpr hgt
+    exact Nat.lt_irrefl (m * l) h_contra
+  apply hall l hlt
+  exists m
+  rwa [Nat.mul_comm]
+
+
+-- theorem min_fac_aux_is_min {n m k: Nat} (hodd: IsOdd k) (hn: 0 < n) (hk: 1 < k) (hm: 1 < m) (h2n: ¬2∣n) (hlt: m < minFacAux n k hk): ¬m ∣ n := by
+--   intro hmn
+--   by_cases heven: IsEven m
+--   case pos =>
+--     rcases heven with ⟨l, hl⟩
+--     exact h2n (Nat.dvd_trans (by exists l) hmn)
+--   case neg =>
+--     have hodd_m := not_even_is_odd m heven
+--     induction k using Nat.strongRecOn with
+--     | ind i hi =>
+--       have hlt': m < minFacAux n (i + 2) (by omega) := sorry -- hlt
+--       exact hi (i + 2) sorry (odd_of_odd_plus_2 hodd) (by omega) hlt'
+
+theorem le_of_le_of_ne {n m: Nat} (h₁: n ≤ m.succ) (h₂: n ≠ m.succ): n ≤ m :=
+  Nat.le_of_lt_succ (Nat.lt_of_le_of_ne h₁ h₂)
+
+theorem le_of_le_sq {n m: Nat} (h: m * m ≤ n): m ≤ n := by
+  match m with
+  | 0 => exact Nat.zero_le n
+  | m + 1 => calc
+      m + 1 ≤ (m + 1) * (m + 1) := Nat.le_mul_of_pos_left (m + 1) (Nat.succ_pos m)
+      _ ≤ n := h
+
+theorem min_fac_aux_is_min {n m k: Nat} (hodd: IsOdd k) (hn: 0 < n) (hk: 1 < k) (hm: 1 < m) (h2n: ¬2∣n) (hinv: ∀i, 1 < i → i ≤ k → ¬i∣n) (hlt: m < minFacAux n (k + 2) (by omega)): ¬m ∣ n := by
+  have hlt_bak := hlt
+  have hk2: 1 < k + 2 := by omega
+  unfold minFacAux at hlt
+  intro hmn
+  have bla: m ≠ k + 1 := by
+    intro heq
+    have xxx: 2 ∣ k + 1 := by
+      rcases hodd with ⟨l, hl⟩
+      rw [hl]
+      exists l + 1
+    rw [←heq] at xxx
+    exact h2n (Nat.dvd_trans xxx hmn)
+  split at hlt
+  . exact hinv m hm (Nat.le_of_lt_succ (Nat.lt_of_le_of_ne (Nat.le_of_lt_succ hlt) bla)) hmn
+  . split at hlt
+    rename n < (k + 2) * (k + 2) => hnk2
+    case isFalse.isTrue hnk2 =>
+      by_cases hmk: m ≤ k
+      . exact hinv m hm hmk hmn
+      . have hinv': ∀i, 1 < i → i ≤ k + 2 → ¬i ∣ n := by
+          intros i hi hilt hin
+          have hink2: i ≠ k + 2 := by intro hieq; rw [hieq] at hin; contradiction
+          have hink: i ≠ k + 1 := by
+            intro hieq
+            have blubb: ¬ k + 1 ∣ n := by
+              intro hdvd
+              apply h2n
+              exact Nat.dvd_trans (even_eq_dvd_two.mp (even_of_odd_succ hodd)) hdvd
+            rw [hieq] at hin
+            contradiction
+
+          have step1: i ≤ k + 1 := le_of_le_of_ne hilt hink2
+          have step2: i ≤ k := le_of_le_of_ne step1 hink
+
+          exact hinv i hi step2 hin
+        exact min_fac_aux_is_min (odd_of_odd_plus_2 hodd) hn (by omega) hm h2n hinv' (Nat.lt_of_lt_of_le hlt_bak (mfa_lt_mfa_succ_succ hn hk2)) hmn
+    case isFalse.isFalse hnnk2 =>
+      have tmp: (k + 2) * (k + 2) ≤ n := by omega
+      have tmp' := le_of_le_sq tmp
+      sorry
+termination_by n - k
+decreasing_by
+  -- This seems somewhat crude ...
+  have : k < k * k := by
+    conv => lhs; rw [←Nat.mul_one k]
+    rwa [Nat.mul_lt_mul_left (by omega)]
+  omega
+
+-- theorem min_fac_aux_is_min {n m: Nat} (k: Nat) (hn: 0 < n) (hm: 1 < m) (hodd: IsOdd k) (h2n: ¬2∣n) (h2m: ¬2∣m) (hk: 1 < k + 2) (hinv: ∀i, 1 < i → i ≤ k → ¬ i ∣ n) (hlt: m < minFacAux n (k + 2) hk): ¬m ∣ n := by
+--   unfold minFacAux at hlt
+--   split at hlt
+--   . intro hmn
+--     rename (k∣n) => hkn
+--     exact hinv m hm (Nat.le_of_lt hlt) hmn
+--   . split at hlt
+--     . intro hmn
+
+--       exact hinv m hm sorry hmn
+--     . have hinv': ∀ (i : Nat), 1 < i → i ≤ k + 2 → ¬i ∣ n := by
+--         intros i hi hilt hin
+--         have hilt': i ≤ k := by
+
+--           sorry
+--         exact hinv i hi hilt' hin
+--       exact min_fac_aux_is_min (k + 2) hn hm h2n h2m (by omega) hinv' hlt
+-- termination_by n - k
+-- decreasing_by
+--   -- This seems somewhat crude ...
+--   have : k < k * k := by
+--     conv => lhs; rw [←Nat.mul_one k]
+--     rwa [Nat.mul_lt_mul_left (by omega)]
+--   omega
+
+theorem min_fac_le_n {n: Nat} (h: 0 < n): minFac n ≤ n :=
+  Nat.le_of_dvd h min_fac_dvd
+
+
+theorem min_fac_is_min {n m: Nat} (hn: 0 < n) (hm: 1 < m): m < minFac n → ¬ m ∣ n := by
+  have hfn := min_fac_le_n hn
+  intro hlt
+  have hm_lt_n := Nat.lt_of_lt_of_le hlt hfn
+  replace hn := Nat.lt_trans hm hm_lt_n
+  unfold minFac at hlt
+  split at hlt
+  . omega
+  . rename (¬2∣n) => h2n
+    by_cases h2m: 2 ∣ m
+    . intro hmn
+      exact h2n (Nat.dvd_trans h2m hmn)
+    . cases min_fac_aux_val n 3 (by omega) with
+      | inl hn' => sorry
+      | inr hn'' => sorry
+
 theorem min_fac_is_prime {n: Nat} (h: 1 < n): Prime (minFac n) := by
   constructor
-  . sorry
+  . exact min_fac_gt_2 h
   . intro m hm hmd
     have hm': m ≤ minFac n := Nat.le_of_dvd (min_fac_pos) hmd
     have hm': m < minFac n := Nat.lt_of_le_of_ne hm' hm.right
-    sorry
+    have hm2: 1 < m := hm.left
+    have bla := min_fac_is_min (by omega) hm2 hm'
+    apply bla
+    exact Nat.dvd_trans hmd min_fac_dvd
+
+
+#eval minFac (factorial 10 + 1)
+
+theorem mul_one_one_of_eq_one {n m: Nat}: n * m = 1 ↔ n = 1 ∧ m = 1 := by
+  constructor
+  . intro h
+    match n, m with
+      | 0, _ => rw [Nat.zero_mul] at h; contradiction
+      | _, 0 => rw [Nat.mul_zero] at h; contradiction
+      | 1, 1 => exact ⟨rfl, rfl⟩
+      | n + 2, m + 2 =>
+        rw [Nat.add_mul] at h
+        simp_arith at h
+  . rintro ⟨hn, hm⟩
+    rw [hn, hm]
+
+theorem not_dvd_succ_of_dvd {n m: Nat} (h_pos: 0 < m) (hn2: 1 < n) (h: n ∣ m): ¬ n ∣ m + 1 := by
+  intro h1
+  rcases h with ⟨k, hk⟩
+  rcases h1 with ⟨l, hl⟩
+  match n with
+  | 0 => rw [Nat.zero_mul] at hk; rw [hk] at h_pos; contradiction
+  | n + 1 =>
+    have hm_lt: m < m + 1 := by omega
+
+    have hlt: k < l := by
+      rw [hl, hk] at hm_lt
+      exact Nat.lt_of_mul_lt_mul_left hm_lt
+
+    have hk_pos: 0 < k := by
+      rw [hk] at h_pos;
+      exact Nat.pos_of_mul_pos_left h_pos
+
+    have hl_pos: 0 < l := by
+      replace h_pos := Nat.lt_succ_of_lt h_pos
+      rw [Nat.succ_eq_add_one, hl] at h_pos;
+      exact Nat.pos_of_mul_pos_left h_pos
+
+    have hkl: 0 < l - k := Nat.sub_pos_iff_lt.mpr hlt
+
+    have hx: 0 = (n + 1) * (l - k) - 1 := calc
+      0 = (n + 1) * l - (m + 1) := by rw [hl, Nat.sub_self]
+      _ = (n + 1) * l - ((n + 1) * k + 1) := by rw [hk]
+      _ = (n + 1) * l - (n + 1) * k - 1 := by rw [Nat.sub_add_eq]
+      _ = (n + 1) * (l - k) - 1 := by rw [Nat.mul_sub_left_distrib]
+
+    replace hx: 1 = (n + 1) * (l - k) := by
+      have bla := Nat.sub_eq_zero_iff_le.mp (Eq.symm hx)
+      cases Nat.lt_or_eq_of_le bla with
+      | inl hlt =>
+        rw [Nat.lt_one_iff] at hlt
+        exfalso
+        have tmp := Nat.mul_pos (Nat.succ_pos n) hkl
+        rw [hlt] at tmp
+        contradiction
+      | inr heq => rw [heq]
+
+    replace hx := (mul_one_one_of_eq_one.mp (Eq.symm hx)).left
+    rw [hx] at hn2
+    exact Nat.lt_irrefl 1 hn2
 
 
 theorem exists_infinite_primes: (∀n, ∃p, n ≤ p ∧ Prime p) := by
   intro n
-  let p := minFac (factorial n + 1)
-  exists p
+  exists minFac (factorial n + 1)
   constructor
-  . sorry
+  . false_or_by_contra
+    rename _ => hgt
+    replace hgt := Nat.gt_of_not_le hgt
+    have h_dvd_0: minFac (factorial n + 1) ∣ factorial n := dvd_factorial_of_le (Nat.le_of_lt hgt) min_fac_pos
+    have h_dvd_1: minFac (factorial n + 1) ∣ factorial n + 1 := min_fac_dvd
+    exact not_dvd_succ_of_dvd zero_lt_factorial (min_fac_gt_2 (by apply Nat.succ_lt_succ; exact zero_lt_factorial)) h_dvd_0 h_dvd_1
   . apply min_fac_is_prime
-    exact Nat.succ_lt_succ_iff.mpr (zero_lt_factorial n)
-    -- constructor
-    -- . sorry
-    -- . intro m
-    --   intro hm
-    --   intro hmp
-    --   have hm_le_fac_n1: m ≤ factorial n :=
-    --     Nat.le_of_lt_succ (Nat.lt_of_le_of_ne (Nat.le_of_dvd (sorry) hmp) hm.right)
-
-    --   sorry
+    exact Nat.succ_lt_succ_iff.mpr (zero_lt_factorial)
